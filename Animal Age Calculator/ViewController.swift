a950a8a75b23f68d12c6594fced8fbec68642c47
remy <remy@remys-mac.local>
2015-09-17 15:24:30 -0400

    Initial Commit

---
+++ Animal Age Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animal Age Calculator
+//
+//  Created by remy on 9/17/15.
+//  Copyright © 2015 LiquidGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
