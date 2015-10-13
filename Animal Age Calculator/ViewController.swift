de7a13cb06b35f4185512d7205384ade7bca5f87
remy <remy@remys-mac.local>
2015-10-13 12:17:03 -0400

    completed application

--- Animal Age Calculator/ViewController.swift
+++ Animal Age Calculator/ViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageField: UITextField!
+    
+    @IBAction func calculateAge(sender: AnyObject) {
+        
+        if ageField.text == nil {
+            
+            calculatedAgeLabel.text = "Please enter a number"
+            
+        } else {
+            
+        var catAge = Int(ageField.text!)!
+        
+        catAge = catAge * 7
+        
+        calculatedAgeLabel.text = "Your cat is \(catAge) in cat years"
+            
+        }
+    }
+    
+    @IBOutlet weak var calculatedAgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
